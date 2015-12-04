34a1b5f6b7f3e08c163ddd55427ee238682cff3a
andrey82 <andrey.pop@gmail.com>
2015-12-04 23:49:42 +0200

    Update pyTecdocData.py

--- pyTecdocData.py
+++ pyTecdocData.py
@@ -2,7 +2,23 @@
 
 import tecdoc
 
-cd = tecdoc.Cd('TECDOC_CD_2_2015', dsn="t_2_15")
+
+#please set your data directory !
+#make sure you have enougth space!
+# DO NOT manualy delete it !
+
+datadir = r'D:\tecdoc_data\data'
+tecdoc.setDataDir(datadir)
+
+
+
+
+#please set uo your DSN in Windows -> Control Panel -> ODBC
+#you have to set remember password, and choose default schema (TECDOC_CD_n_nnnn)
+#you should also change teccd to be the default schema !
+#dsn is the name you give to your connection in Control Panel -> ODBC !
+teccd = 'TECDOC_CD_2_2015'
+cd = tecdoc.Cd(teccd, dsn="t_2_15")
 print(cd)
 
 cd.loadTablesFromDB()
